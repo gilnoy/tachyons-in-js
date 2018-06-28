76b0edaf6797cb7108132baf10791d9fc0af96ea
Gil <gil@cloudinary.com>
2018-06-28 21:00:53 +0300

    reorganize folders structure

--- stories/index.js
+++ stories/index.js
@@ -4,14 +4,14 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import glamorous from 'glamorous';
-import tc from '../src/index';
+import tc from '../index';
 
 
 storiesOf('Glamorous', module)
-  .add('with text', () => {
-    const Href = glamorous.a({
-      ...tc('flex tc pa3 bg-purple white h4 w4 f3 bg-red-m yellow-m hover-bg-green')
-    });
-    return <Href>Hello Tachyons</Href>;
-  })
+.add('with text', () => {
+  const Href = glamorous.a({
+    ...tc('flex tc pa3 bg-purple white h4 w4 f3 bg-red-m yellow-m hover-bg-green')
+  });
+  return <Href>Hello Tachyons</Href>;
+})
   
